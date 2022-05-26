cd3a25b36a209c3b9445ea37a45ebe09be5bbbe5
Angelina <pretzel318@gmail.com>
2022-05-26 13:39:02 +0400

    added header & cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          <img width={40} height={40} src="/img/logo.png" alt=""/>
+          <div>
+            <h3 className="text-uppercase">React Sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="d-flex align-center">
+          <li className="mr-30 d-flex align-center">
+            <img width={18} height={18} src="/img/cart.svg" alt="" />
+            <span>1205 руб.</span>
+          </li>
+          <li className="mr-30 d-flex align-center">
+            <img width={18} height={18} src="/img/favourite.svg" alt=""/>
+          </li>
+          <li className="mr-30 d-flex align-center">
+            <img width={18} height={18} src="/img/user.svg" alt=""/>
+          </li>
+        </ul>
       </header>
+      <div className="content p-40">
+        <h1 className="mb-40">Все кроссовки</h1>
+        <div className="d-flex justify-between flex-wrap">
+          <div className="card">
+            <img width={133} height={112} alt="" src="/img/sneakers/1.png" />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span className="cardPrice">Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img width={133} height={112} alt="" src="/img/sneakers/2.png" />
+            <h5>Мужские Кроссовки Nike Air Max 270</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span className="cardPrice">Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img width={133} height={112} alt="" src="/img/sneakers/3.png" />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span className="cardPrice">Цена:</span>
+                <b>8 499 руб.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img width={133} height={112} alt="" src="/img/sneakers/4.png" />
+            <h5>Кроссовки Puma X Aka Boku Future Rider</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span className="cardcardPrice">Цена:</span>
+                <b>8 999 руб.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
